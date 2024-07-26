a4fb452478b1e2320c0ee7cec8f264ad66435004
Sanghoon Lee <gcccompil3r@gmail.com>
2024-07-26 14:12:13 +0900

    add: 클라이언트 데이터 수신

--- receiver/repository/receiver_repository.py
+++ receiver/repository/receiver_repository.py
@@ -5,3 +5,7 @@ class ReceiverRepository(ABC):
     @abstractmethod
     def injectClientSocket(self, clientSocket):
         pass
+
+    @abstractmethod
+    def receive(self, clientSocketObject):
+        pass
