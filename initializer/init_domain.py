01eb7005d9b2b0554617f75783aa93d792e759c2
Sanghoon Lee <gcccompil3r@gmail.com>
2024-07-26 11:46:53 +0900

    add: Acceptor & Receiver Domain 초기화

--- initializer/init_domain.py
+++ initializer/init_domain.py
@@ -1,3 +1,5 @@
+from acceptor.service.socket_accept_service_impl import SocketAcceptServiceImpl
+from receiver.service.receiver_service_impl import ReceiverServiceImpl
 from server_socket.service.server_socket_service_impl import ServerSocketServiceImpl
 from task_worker.service.task_worker_service_impl import TaskWorkerServiceImpl
 
@@ -12,6 +14,17 @@ class DomainInitializer:
         ServerSocketServiceImpl.getInstance()
 
     @staticmethod
+    def initSocketAcceptDomain():
+        SocketAcceptServiceImpl.getInstance()
+
+    @staticmethod
+    def initReceiverDomain():
+        ReceiverServiceImpl.getInstance()
+
+    @staticmethod
     def initEachDomain():
         DomainInitializer.initTaskWorkerDomain()
         DomainInitializer.initServerSocketDomain()
+        DomainInitializer.initSocketAcceptDomain()
+        DomainInitializer.initReceiverDomain()
+
