af104fc91ad81cf5919b5ff47b0e806409726469
Sanghoon Lee <gcccompil3r@gmail.com>
2024-07-26 17:21:57 +0900

    fix: 한글 깨짐 문제 처리

--- transmitter/service/transmitter_service_impl.py
+++ transmitter/service/transmitter_service_impl.py
@@ -61,7 +61,7 @@ class TransmitterServiceImpl(TransmitterService):
                 willTransmitData = self.checkTransmitChannelData()
                 ColorPrinter.print_important_data("will transmit data", f"{willTransmitData}")
 
-                serializedTransmitData = json.dumps({"message": willTransmitData})
+                serializedTransmitData = json.dumps({"message": willTransmitData}, ensure_ascii=False)
                 self.__transmitterRepository.transmit(clientSocketObject, serializedTransmitData)
 
             except socket.error as socketException:
